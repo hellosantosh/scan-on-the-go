674ff313e24dceaa148a5471dfaca3d3dbf2213f
Smita Chaugule <smita@Smitas-MacBook-Air.local>
2017-12-13 13:47:39 +0530

    submit picture

--- src/environment.ts
+++ src/environment.ts
@@ -1,39 +1,27 @@
-export const environment = {
-    firebaseConfig: {
-        apiKey: "AIzaSyBNpXcigoFbfpfdwWRE4s9uYG0jgWGsdFQ",
-        authDomain: "myexpenses-integ-1505403894550.firebaseapp.com",
-        databaseURL: "https://myexpenses-integ-1505403894550.firebaseio.com",
-        projectId: "myexpenses-integ-1505403894550",
-        storageBucket: "myexpenses-integ-1505403894550.appspot.com",
-        messagingSenderId: "969734073986"
-    },
-    googleCloudVisionAPIKey: "AIzaSyANAN0EjOgxDrFFhsSi8DZfzp79KUiR7us"
-
-};
-
-
 // export const environment = {
 //     firebaseConfig: {
-//       apiKey: "AIzaSyDNKvu5YZ6fFQozFWWTWp3cykmGjv2qUyo",
-//       authDomain: "scan-on-the-go-188706.firebaseapp.com",
-//       databaseURL: "https://scan-on-the-go-188706.firebaseio.com",
-//       projectId: "scan-on-the-go-188706",
-//       storageBucket: "scan-on-the-go-188706.appspot.com",
-//       messagingSenderId: "859434171449"
-
-
+//         apiKey: "AIzaSyBNpXcigoFbfpfdwWRE4s9uYG0jgWGsdFQ",
+//         authDomain: "myexpenses-integ-1505403894550.firebaseapp.com",
+//         databaseURL: "https://myexpenses-integ-1505403894550.firebaseio.com",
+//         projectId: "myexpenses-integ-1505403894550",
+//         storageBucket: "myexpenses-integ-1505403894550.appspot.com",
+//         messagingSenderId: "969734073986"
 //     },
-//     googleBucketConfig: {
-//         apiKey: "AIzaSyANAN0EjOgxDrFFhsSi8DZfzp79KUiR7us",
-//         authDomain: "scan-on-the-go-188706.firebaseapp.com",
-//         databaseURL: "https://www.googleapis.com/download/storage/v1/b/in2017sgobuk",
-//         projectId: "scan-on-the-go-188706",
-//         storageBucket: "scan-on-the-go-188706.appspot.com",
-//         messagingSenderId: "859434171449"
+//     googleCloudVisionAPIKey: "AIzaSyANAN0EjOgxDrFFhsSi8DZfzp79KUiR7us"
+
+// };
 
 
-//       }, 
+export const environment = {
+    firebaseConfig: {
+      apiKey: "AIzaSyDNKvu5YZ6fFQozFWWTWp3cykmGjv2qUyo",
+      authDomain: "scan-on-the-go-188706.firebaseapp.com",
+      databaseURL: "https://scan-on-the-go-188706.firebaseio.com",
+      projectId: "scan-on-the-go-188706",
+      storageBucket: "scan-on-the-go-188706.appspot.com",
+      messagingSenderId: "859434171449"
 
-//     googleCloudVisionAPIKey: "AIzaSyBAdXjvER-3qVyiSuxnyJjMz2pLr5nSJn8"
+    },
+    googleCloudVisionAPIKey: "AIzaSyBAdXjvER-3qVyiSuxnyJjMz2pLr5nSJn8"
 
-//   };
+  };
